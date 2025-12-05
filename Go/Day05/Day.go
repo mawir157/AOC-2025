ba3dde35205c31b471e901e1fb1c586465513486
mawir157 <jamesmthompson2@gmail.com>
2025-12-05 18:16:46 +0000

    [05] 🥚🥚

--- Go/Day05/Day.go
+++ Go/Day05/Day.go
@@ -36,26 +36,7 @@ func parseInput(ss []string) ([]Interval, []int) {
 	return rs, ns
 }
 
-func checkIngredients(rs []Interval, ns []int) int {
-	allGood := 0
-
-	for _, n := range ns {
-		good := false
-		for _, i := range rs {
-			if i.lo <= n && n <= i.hi {
-				good = true
-				break
-			}
-		}
-		if good {
-			allGood++
-		}
-	}
-
-	return allGood
-}
-
-func checkIngredientsAlt(rs []Interval) int {
+func checkIngredients(rs []Interval, ns []int) (int, int) {
 	sort.Slice(rs, func(i, j int) bool {
 		return rs[i].lo < rs[j].lo
 	})
@@ -68,10 +49,8 @@ func checkIngredientsAlt(rs []Interval) int {
 		if A.hi < B.lo { // no overlap insert A and move onto B
 			merged_rs = append(merged_rs, A)
 			A = B
-		} else if A.hi > B.hi { // A completely contains B, so just keep A
-			continue
-		} else { // A and B over lap so merge
-			A = Interval{A.lo, B.hi}
+		} else {
+			A.hi = AH.Max(A.hi, B.hi)
 		}
 	}
 	// remember to add final interval
@@ -82,15 +61,27 @@ func checkIngredientsAlt(rs []Interval) int {
 		total += r.hi - r.lo + 1
 	}
 
-	return total
+	allGood := 0
+
+	for _, n := range ns {
+		for _, i := range merged_rs {
+			if i.lo <= n && n <= i.hi {
+				allGood++
+				break
+			}
+		}
+	}
+
+	return allGood, total
+
+	// return total
 }
 
 func Run() {
 	is, _ := AH.ReadStrFile("../inputs/day05.txt")
 	rs, ns := parseInput(is)
 
-	p1 := checkIngredients(rs, ns)
-	p2 := checkIngredientsAlt(rs)
+	p1, p2 := checkIngredients(rs, ns)
 
 	AH.PrintSoln(5, p1, p2)
 
